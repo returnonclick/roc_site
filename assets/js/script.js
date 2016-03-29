15c5b09425a644fa40eae3fdca6377bbd2fad939
CSALucasNascimento <csalucasnascimento@gmail.com>
2016-03-29 19:05:20 +1100

    js to submit the request by AJAX

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,6 +188,69 @@
   // });
 
 
+    /**
+     * AJAX FUNCTION TO CALL PHP
+     *
+     * BY LUCAS NASCIMENTO
+     *
+     */
+
+    var request;
+    $(document).ready(function(){
+      $("#form-request-call").submit(function(){
+
+        // Abort any pending request
+        if (request) {
+          request.abort();
+        }
+
+        // setup some local variables
+        var $form = $(this);
+        // Let's select and cache all the fields
+        var $inputs = $form.find("input, select, button, textarea");
+        // Serialize the data in the form
+        var serializedData = $form.serialize();
+
+        // Disabled form elements will not be serialized.
+        $inputs.prop("disabled", true);
+
+        // Fire off the request to /form.php
+        request = $.ajax({
+          url: "/request.php",
+          type: "post",
+          data: serializedData
+        });
+
+        // Callback handler that will be called on success
+        request.done(function (response, textStatus, jqXHR){
+          // Log a message to the console
+          console.log("Hooray, it worked!");
+        });
+
+        // Callback handler that will be called on failure
+        request.fail(function (jqXHR, textStatus, errorThrown){
+          // Log the error to the console
+          console.error(
+              "The following error occurred: "+
+              textStatus, errorThrown
+          );
+        });
+
+        // Callback handler that will be called regardless
+        // if the request failed or succeeded
+        request.always(function () {
+          // Reenable the inputs
+          $inputs.prop("disabled", false);
+        });
+
+        // Prevent default posting of form
+        event.preventDefault();
+
+      });
+    });
+
+
+
 })(jQuery);
 
 
