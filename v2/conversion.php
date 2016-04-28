4f02656e752fa82fbd01618f1f3f44e5231eca35
CSALucasNascimento <csalucasnascimento@gmail.com>
2016-04-28 10:27:06 +1000

    test send email

--- v2/conversion.php
+++ v2/conversion.php
@@ -24,8 +24,8 @@ $mail->Port = 465;                                    // TCP port to connect to
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';   // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'lucas@returnonclick.com.au';       // SMTP username
-$mail->Password = 'Aus.2016';                    // SMTP password
+$mail->Username = 'jossandro@gmail.com';       // SMTP username
+$mail->Password = 'kaewqkrghjrybylx';                    // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;
 
