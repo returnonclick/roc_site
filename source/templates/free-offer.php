3d09fbd575c573e564b58de56662f4ca25894f24
CSALucasNascimento <csalucasnascimento@gmail.com>
2016-06-01 12:11:30 +1000

    new path php

--- source/templates/free-offer.php
+++ source/templates/free-offer.php
@@ -5,7 +5,7 @@
                 <h1 class="thick">Turn Your Website Into a Lead-Generating Machine</h1>
             </div>
             <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12">
-                <a href="./contact.html" class="btn btn-services btn-blue">TALK TO A SPECIALIST</a>
+                <a href="./contact.php" class="btn btn-services btn-blue">TALK TO A SPECIALIST</a>
             </div>
         </div>
     </div>
