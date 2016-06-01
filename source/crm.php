42cb4446a5c6cc26fba01bde542af2a43eb55c3a
CSALucasNascimento <csalucasnascimento@gmail.com>
2016-06-01 12:07:39 +1000

    new head

--- source/crm.php
+++ source/crm.php
@@ -1,59 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Return on Click</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="">
-    <meta name="description" content="">
-    <link rel="canonical" href="">
-    <link rel="shortlink" href="">
-    <link rel="author" href="">
-    <meta property="og:site_name" content="Return on Click">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="http://www.returnonclick.com.au">
-    <meta property="og:title" content="Return on Click">
-    <meta property="og:description" content="">
-    <meta property="og:updated_time" content="">
-    <meta property="og:image" content="">
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="@returnonclick">
-    <meta name="twitter:site:id" content="">
-    <meta name="twitter:url" content="http://www.returnonclick.com.au">
-    <meta name="twitter:title" content="Return on Click">
-    <meta name="twitter:description" content="A">
-    <meta name="twitter:image:src" content="">
-    <meta name="twitter:image:width" content="1500">
-    <meta name="twitter:image:height" content="1500">
-    <link rel="apple-touch-icon-precomposed" href="">
-    <link rel="icon" type="image/png" sizes="32x32" href="../docs/images/favicon-32x32.png">
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="../components/bootstrap/css/bootstrap.min.css">
-
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="../components/font-awesome/css/font-awesome.min.css">
-
-    <!-- JQUERY -->
-    <script src="../components/jquery/jquery-2.2.3.min.js"></script>
-
-    <!-- main CSS -->
-    <link rel="stylesheet" href="../docs/styles/main.css">
-    <!-- <link rel="stylesheet" href="../docs/styles/animate.css">
-    <link rel="stylesheet" href="../docs/styles/icons.css"> -->
-
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-55878006-1', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-
+    <?php include './templates/head.php'; ?>
 </head>
 
 <body>
