9c68766bf97dc107cdefecd7c9f990715ff3000f
Jossandro Balardin <jossandro@gmail.com>
2016-11-18 16:50:50 +1100

    donwload online and merge local

---
+++ source/php/report.php
@@ -0,0 +1,49 @@
+<?php
+
+require './mailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'teamreturnonclick@gmail.com';                 // SMTP username
+$mail->Password = 'theBrazilian3';                          // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->addAddress('barrett@returnonclick.com.au', 'Barrett');
+$mail->addBCC('lucas@returnonclick.com.au', 'Lucas');
+$mail->addBCC('jossandro@gmail.com', 'Jossandro');
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'ROC -> Free Report';
+
+$data = json_decode($_POST['data']);
+
+var_dump($data);
+
+$mail->Body    = '
+	<h1>New lead from ROC website</h1>
+	<p>Contact Name: '.$data->contactName.' </p>
+	<p>Email: '.$data->email.' </p>
+	<p>Phone: '.$data->phone.' </p>
+	<p>Website: '.$data->website.' </p>
+';
+
+$mail->AltBody = 'New lead from ROC website
+	Contact Name: '.$data->contactName.'
+	Email: '.$data->email.'
+	Phone: '.$data->phone.'
+	Website: '.$data->website.'
+';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
---
+++ source/templates/seo-report.php
@@ -0,0 +1,44 @@
+    <div class="five-things">
+        <div class="row">
+            <div class="container">
+
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="row row-centered">
+                        <h1 class="">Use our free audit tool </h1>
+                        <h1 class="thick-no-padding">to address issues on your site.</h1>
+                    </div>
+                    <div class="row row-centered">
+                        <div class="col-md-8 col-md-offset-2 col-sm-8 com-sm-offset-2 com-xs-12">
+                            <a id="btn-get-free-report" class="btn btn-blue">
+                                Get My Free Audit
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container form-free-report">
+
+        <div class="offset-md-1 col-md-7 col-sm-5 col-xs-12">
+            <div class="row row-centered">
+                <h1 style="margin-bottom: 0px;">Start free Audit</h1>
+                <div id="form-report" class="col-md-12 col-sm12 col-xs-12" >
+                    <div class="row">
+                        <div class="form-group col-md-12 col-sm-12 col-xs-12">
+                            <!-- Begin MySiteAuditor -->
+                            <script src="//cdn.mysiteauditor.com/audit-tool.js" type="text/javascript"></script>
+                            <div id="seogroup-embed"  data-apikey="b4e414141ff37e50877af0a208aff5b0db0e87a7" data-language="english" data-type="responsive"  ></div>
+                            <!-- End MySiteAuditor -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-5 col-sm-5 col-xs-12">
+            <div class="img-seo-report">
+            </div>
+        </div>
+    </div>
